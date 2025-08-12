636b00529b2deaef74f9c2df3a9772d34b0cb745
thanthtooaung <thanthtoo1285@gmail.com>
2025-08-12 11:12:12 +0700

    :sparkles: feat: added Welcome Controller

--- target/generated-sources/annotations/com/movie/celestix/features/categories/mapper/CategoryMapperImpl.java
+++ target/generated-sources/annotations/com/movie/celestix/features/categories/mapper/CategoryMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-12T11:00:39+0700",
+    date = "2025-08-12T11:11:42+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.7 (Ubuntu)"
 )
 @Component
